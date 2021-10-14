a870d2f286baae87274a873e867bf9a00e2c1f82
Derek Bracken <derekbracken@gmail.com>
2021-10-14 10:54:49 +0100

    Customer Controller done

--- src/main/java/com/example/BookingSystem/controllers/CustomerController.java
+++ src/main/java/com/example/BookingSystem/controllers/CustomerController.java
@@ -1,4 +1,34 @@
 package com.example.BookingSystem.controllers;
 
+
+import com.example.BookingSystem.models.Customer;
+import com.example.BookingSystem.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping(value = "/customers")
+    public ResponseEntity<List<Customer>> getAllCustomers(){
+        return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/customers/{id}")
+    public ResponseEntity getCustomer(@PathVariable Long id){
+        return new ResponseEntity<>(customerRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/customers")
+    public ResponseEntity<Customer> postCustomer(@RequestBody Customer customer){
+        customerRepository.save(customer);
+        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    }
 }
