70505b1d9a7c7ba3b0bf2acee44a940a43022857
Derek Bracken <derekbracken@gmail.com>
2021-10-14 11:01:27 +0100

    Booking Controller done

--- src/main/java/com/example/BookingSystem/controllers/BookingController.java
+++ src/main/java/com/example/BookingSystem/controllers/BookingController.java
@@ -1,9 +1,33 @@
 package com.example.BookingSystem.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.BookingSystem.models.Booking;
+import com.example.BookingSystem.repositories.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BookingController {
 
+    @Autowired
+    BookingRepository bookingRepository;
 
+    @GetMapping(value = "/bookings")
+    public ResponseEntity<List<Booking>> getAllBookings(){
+        return new ResponseEntity<>(bookingRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/bookings/{id}")
+    public ResponseEntity getBooking(@PathVariable Long id){
+        return new ResponseEntity<>(bookingRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/bookings")
+    public ResponseEntity<Booking> postBooking(@RequestBody Booking booking){
+        bookingRepository.save(booking);
+        return new ResponseEntity<>(booking, HttpStatus.CREATED);
+    }
 }
