7baa44f23aa921c965423ae1e478fbe37b372bba
Derek Bracken <derekbracken@gmail.com>
2021-10-14 11:38:03 +0100

    DataLoader populated

--- src/main/java/com/example/BookingSystem/components/DataLoader.java
+++ src/main/java/com/example/BookingSystem/components/DataLoader.java
@@ -1,4 +1,83 @@
 package com.example.BookingSystem.components;
 
-public class DataLoader {
+import com.example.BookingSystem.models.Booking;
+import com.example.BookingSystem.models.Course;
+import com.example.BookingSystem.models.Customer;
+import com.example.BookingSystem.repositories.BookingRepository;
+import com.example.BookingSystem.repositories.CourseRepository;
+import com.example.BookingSystem.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    public DataLoader() {
+    }
+
+    public void run(ApplicationArguments args){
+
+        Customer jack = new Customer("Jack Jobs", "Dublin", 34);
+        customerRepository.save(jack);
+
+        Customer jill = new Customer("Jill Hibs", "Edinburgh", 34);
+        customerRepository.save(jill);
+
+        Customer lilly = new Customer("Lilly Filly", "Edinburgh", 34);
+        customerRepository.save(lilly);
+
+        Customer ben = new Customer("Ben Hem", "Dublin", 34);
+        customerRepository.save(ben);
+
+        Customer nick = new Customer("Nick Slick", "Cork", 34);
+        customerRepository.save(nick);
+
+
+        Course python = new Course("Introduction to Python", "Dublin", 4);
+        courseRepository.save(python);
+
+        Course java = new Course("Introduction to java", "Dublin", 5);
+        courseRepository.save(java);
+
+        Course photoShop = new Course("Introduction to Photo Shop", "Edinburgh", 3);
+        courseRepository.save(photoShop);
+
+        Course animation = new Course("Introduction to Animation", "Edinburgh", 2);
+        courseRepository.save(animation);
+
+        Course webDev = new Course("Introduction to Web Development", "Edinburgh", 1);
+        courseRepository.save(webDev);
+
+
+
+        Booking booking1 = new Booking("20-10-21", python, jack);
+        bookingRepository.save(booking1);
+
+        Booking booking2 = new Booking("21-10-21", java, jill);
+        bookingRepository.save(booking2);
+
+        Booking booking3 = new Booking("22-11-21", photoShop, ben);
+        bookingRepository.save(booking3);
+
+        Booking booking4 = new Booking("23-10-21", animation, lilly);
+        bookingRepository.save(booking4);
+
+        Booking booking5 = new Booking("24-11-21", webDev, nick);
+        bookingRepository.save(booking5);
+
+        
+    }
+
+
 }
